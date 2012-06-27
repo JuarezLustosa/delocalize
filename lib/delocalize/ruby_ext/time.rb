06c324cded9f8c69b1dc32e136c8269d201ed0b3
Juarez Lustosa <juarez.lustosa@gmail.com>
2012-06-27 17:34:56 -0300

    Add method valid? to validate parse, like Date.valid_localized? and DateTime.valid_localized?

--- lib/delocalize/ruby_ext/time.rb
+++ lib/delocalize/ruby_ext/time.rb
@@ -5,5 +5,9 @@ Time.class_eval do
     def parse_localized(time)
       Delocalize::LocalizedDateTimeParser.parse(time, self)
     end
+
+    def valid_localized?(datetime)
+      Delocalize::LocalizedDateTimeParser.valid?(datetime, self)
+    end
   end
 end
